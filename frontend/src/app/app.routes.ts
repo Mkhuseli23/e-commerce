3da490c9a819cc194f42d1e6ffb0fa5c3af7eae2
Mkhuseli23 <mkhuselimditshwa23@gmail.com>
2025-05-09 11:27:19 +0200

    maziwe

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
-import { LoginComponent } from './login/login.component';
 import { CartComponent } from './cart/cart.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutUsComponent } from './about-us/about-us.component';
@@ -13,7 +11,5 @@ export const routes: Routes = [
   { path: 'cart', component: CartComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'about-us', component: AboutUsComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminDashboardComponent },
   { path: '**', redirectTo: '' }
 ];
